693cbbc78ca2ede735a29259e8098e945b9e8b08
Saiful Ihsan <31265608+MSaifulIhsan@users.noreply.github.com>
2018-07-18 23:19:59 +0700

    Add files via upload

---
+++ kalkulator.cpp
@@ -0,0 +1,19 @@
+class Barang {
+	public:
+		int harga;
+};
+
+class Hitung:public Barang{
+	int jml;
+
+	public:
+		void set_values (int jml, int harga);
+		int smtr(){
+			return jml * harga;
+		}
+};
+
+void Hitung::set_values (int j, int h) {
+	jml = j;
+	harga = h;
+}
